94a39165a299e4e3d354e03988957dbe65e155ab
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-10-25 09:19:28 +0900

    演習問題-03を追加

---
+++ Ex-02-03/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("数値を入力してください->");
+int value = int.Parse(Console.ReadLine());
+int value1 = value + 5;
+int value2 = value - 5;
+Console.WriteLine("5を加算した結果は" + value1+"です。");
+Console.WriteLine("5を減算した結果は" + value2+"です。");
